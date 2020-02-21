70329840f813328147118021959c790dce3164a5
Norum Eidesen <Hans.Kristian.Eidesen@dnvgl.com>
2020-02-21 08:25:25 +0100

    Added test to check if deviations

--- run.py
+++ run.py
@@ -1,23 +1,30 @@
 import test
 import classifier
-from flask import Flask, request, jsonify
+import flask
 from flask_restful import Resource, Api
+import json
+from flask import jsonify
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 api = Api(app)
 
 @app.route("/health", methods=["GET"])
 def health():
     content = ""
     status_code = 200
+    print("the content is: ", content)
     return content, status_code
 
 @app.route("/classification", methods=["POST"])    
 def classification():
     status_code = 200
-    report_url = "2019/conocophillips-ekofisk-stimuleringsoperasjon-fra-fartoy/"
-    return classifier.main(report_url), status_code
-#api.add_resource(classification, '/classification/<report_url>')
+    data = flask.request.get_json()
+
+    #accessing the part after ?id= in the URL
+    report_url = flask.request.args.get('id')
+    print("The POST request is: ",flask.request.args.get('id'))
+    return jsonify(classifier.main(report_url)), status_code
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
